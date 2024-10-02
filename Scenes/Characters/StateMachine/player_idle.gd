f8572fb305bb7bf2670ca84b5a3e006538977036
diegoxgarcia <diegoxgarcia@gmail.com>
2024-10-02 10:07:29 -0300

    Update player_idle.gd

--- Scenes/Characters/StateMachine/player_idle.gd
+++ Scenes/Characters/StateMachine/player_idle.gd
@@ -2,8 +2,11 @@ class_name PlayerIdle
 extends State
 
 
-func enter():	
+func enter():
+	# Estar en el lugar con la animacion Idle, la velocidad tiene que estar en 0, y sin tener acciones previas
 	pass
 	
 func update(delta : float):
+	
 	pass
+	
